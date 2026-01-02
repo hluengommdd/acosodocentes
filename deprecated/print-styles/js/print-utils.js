8405bbbb7ef250937ccb2a6bb8f59cc453826602
hluengommdd <hluengo@mmddconcepcion.cl>
2026-01-02 11:10:06 +0000

    chore: remove deprecated assets

--- deprecated/print-styles/js/print-utils.js
+++
@@ -1,6 +0,0 @@
-// Helper utilities for the print view. Keep small and dependency-free.
-(function(){
-  window.printUtils = {
-    safeJoin: function(arr, sep){ return Array.isArray(arr) ? arr.join(sep||', ') : '' }
-  }
-})();
